3f5247cc90efc16b730fd32c3c1a7ed62d44435d
richsueh <richardhsueh94@gmail.com>
2020-07-23 17:26:04 +0800

    add react-router example

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import LanguageButton from '../LanguageButton';
 import './index.scss';
+import { Switch, Route, Link } from 'react-router-dom';
 
 const shimmer = keyframes`
   0%{background-position:0% 50%}
@@ -18,7 +19,7 @@ const H1 = styled.span`
   display: block;
   margin: 0;
   font-size: 7rem;
-  line-height: 1;
+  line-height: 8.5rem;
   font-weight: 900;
   text-align: center;
   background-clip: text;
@@ -30,13 +31,44 @@ const H1 = styled.span`
   overflow-wrap: break-word;
 `;
 
+const Button = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-weight: normal;
+`;
+
 const App = () => {
   const { t } = useTranslation('translation');
 
   return (
     <div>
       <LanguageButton />
-      <H1>React. Typescript. Webpack. {t('welcome')}</H1>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <>
+                <H1>
+                  React. Typescript. Webpack.
+                  <br /> {t('welcome')}
+                </H1>
+                <Button to="/page2">Go to Page 2</Button>
+              </>
+            );
+          }}
+        />
+        <Route
+          path="/page2"
+          render={() => (
+            <>
+              <H1>{t('welcome')} Page 2</H1>
+              <Button to="/">Go to Homepage</Button>
+            </>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
